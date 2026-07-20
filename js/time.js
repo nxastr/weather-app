2d39760efc7b1c32220c08125ea7a7ba06e4bc3d
Zuzanna Chmielewska <zuzannna.chmielewska@gmail.com>
2026-07-20 16:57:34 +0200

    Initial commit

---
+++ js/time.js
@@ -0,0 +1,38 @@
+function updateDateTime() {
+
+// create a new `Date` object
+const now = new Date();
+
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
+// get the current date and time as a string
+const currentHour = hours.toLocaleString();
+const currentMinutes = minutes.toLocaleString();
+const currentseconds = seconds.toLocaleString();
+
+
+//chceck if the number is from 1 to 9 and add 0 at the begining
+//update the `textContent` property of the `span` element with the `id` of `datetime`
+
+if (hours < 10) {
+    document.querySelector('#hours').textContent = "0" + currentHour;
+} else {
+    document.querySelector('#hours').textContent = currentHour;
+}
+if (minutes < 10) {
+    document.querySelector('#minutes').textContent = "0" + currentMinutes;
+} else {
+    document.querySelector('#minutes').textContent = currentMinutes;
+}
+if (seconds < 10) {
+    document.querySelector('#seconds').textContent = "0" + currentseconds;
+} else {
+    document.querySelector('#seconds').textContent = currentseconds;
+}
+
+}
+    
+// call the `updateDateTime` function every second
+setInterval(updateDateTime, 1000);
